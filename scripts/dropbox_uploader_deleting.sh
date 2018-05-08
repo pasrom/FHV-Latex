9bcc03acb55f6e8ab3cf423b0bb9d5234db4524e
pasrom <32713472+pasrom@users.noreply.github.com>
2018-05-08 21:22:24 +0200

    adding submodule for dropbox uploader

--- scripts/dropbox_uploader_deleting.sh
+++ scripts/dropbox_uploader_deleting.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./dropbox_uploader.sh list | grep "pdf" | awk '{print $3}' | while read line
+./scripts/Dropbox-Uploader/dropbox_uploader.sh list | grep "pdf" | awk '{print $3}' | while read line
 do
- ./dropbox_uploader.sh delete "$line"
+ ./scripts/Dropbox-Uploader/dropbox_uploader.sh delete "$line"
 done
